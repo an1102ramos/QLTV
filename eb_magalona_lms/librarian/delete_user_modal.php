f9171221d49031cf5bdcffeeb2d435049ea609eb
tranvanan <an1102ramos@gmail.com>
2021-05-22 23:19:04 +0700

    up

---
+++ eb_magalona_lms/librarian/delete_user_modal.php
@@ -0,0 +1,11 @@
+	<div id="delete_user<?php echo $id; ?>" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-body">
+			<div class="alert alert-danger">Bạn có chắc chắn muốn xóa?</div>
+
+		</div>
+		<div class="modal-footer">
+			<a class="btn btn-danger" href="delete_user.php<?php echo '?id='.$id; ?>"><i class="icon-check"></i>&nbsp;Đồng ý</a>
+			<button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove icon-large"></i>&nbsp;Đóng</button>
+		</div>
+    </div>
+	
