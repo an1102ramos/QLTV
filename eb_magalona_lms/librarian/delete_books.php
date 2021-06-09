539d4fdb72383f11b2b8d39270ab2f40ae9d2f6b
tranvanan <an1102ramos@gmail.com>
2021-06-09 11:22:41 +0700

    Again

--- eb_magalona_lms/librarian/delete_books.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('dbcon.php');
-$id=$_GET['id'];
-mysqli_query($conn,"update book set status = 'Archive' where book_id='$id'")or die(mysqli_error());
-header('location:books.php');
-?>
