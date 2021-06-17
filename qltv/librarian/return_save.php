372e92ba31132cc0c0884829b3e099761d7d0127
tranvanan <an1102ramos@gmail.com>
2021-06-17 11:55:24 +0700

    Update return_save.php

--- qltv/librarian/return_save.php
+++ qltv/librarian/return_save.php
@@ -4,7 +4,8 @@ include('dbcon.php');
 $id=$_GET['id'];
 $book_id = $_GET['book_id'];
 
-mysqli_query($conn,"update borrow LEFT JOIN borrowdetails on borrow.borrow_id = borrowdetails.borrow_id   set borrow_status='Đã trả',date_return= NOW() where borrow.borrow_id='$id' and borrowdetails.book_id = '$book_id'")or die(mysqli_error());						
+mysqli_query($conn,"update borrow LEFT JOIN borrowdetails on borrow.borrow_id = borrowdetails.borrow_id   set borrow_status='Đã trả',date_return= NOW() where borrow.borrow_id='$id' and borrowdetails.book_id = '$book_id'")or die(mysqli_error());	
+mysqli_query($conn,"update book set book_copies = book_copies +1 where book_id=$book_id ") or die(mysqli_error());					
  header('location:view_borrow.php');
 
 ?>	
